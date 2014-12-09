84211cf80eddd42edc6c288f3630b0ab237a0f07
arosenberg01 <ansel01@gmail.com>
2014-12-09 03:39:37 -0800

    Hot or Cold App - Complete (w/ better form validation)

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ $(document).ready(function(){
 	  		return Math.floor(Math.random() * 100 + 1);
 	}
 
+	/*--- Start game over ---*/
+
 	function newGame() {
 		secretNum = generateNum();
 		$('#count').text(0)
@@ -14,31 +16,39 @@ $(document).ready(function(){
 		console.log("Psst. The secret number is " + secretNum);
 	}
 
+	/*--- Generates correct feedback based on user input --- */
+
 	function feedback(userinput) {
 
 	    oldGuess = parseInt($('#guessList li').last().html());
 
 	    guessCount = parseInt($('#count').text());
 
-		if (userinput > 0 && userinput < 101) {
+		if (isNaN(userinput)) { 
+			$("#feedback").text("I need a number between 1-100");
+		  	$('#userGuess').val('');
+
+		} else if (parseInt(userinput) > 0 && parseInt(userinput) < 101) {
+
+			userinput = parseInt(userinput);
 
 			if (guessCount === 0) {
 
 				userinput > secretNum ? diff = userinput - secretNum : diff = secretNum - userinput;
 
 				if (secretNum === userinput) {
-			  			$("#feedback").text("You guessed it! It's " + secretNum);
-			  		} else if (diff < 10) {
-			  			$("#feedback").text("Very hot");
-			  		} else if (diff < 20) {
-			  			$("#feedback").text("Hot");
-			  		} else if (diff < 30) {
+			  		$("#feedback").text("You guessed it! It's " + secretNum);
+			  	} else if (diff < 10) {
+			  		$("#feedback").text("Very hot");
+			  	} else if (diff < 20) {
+			  		$("#feedback").text("Hot");
+			  	} else if (diff < 30) {
 			  			$("#feedback").text("Warm");
-			  		} else if (diff < 50) {
+			  	} else if (diff < 50) {
 			  			$("#feedback").text("Cold");
-			  		} else {
+			  	} else {
 			  			$("#feedback").text("Ice cold");
-			  		}
+			  	}
 
 				$('#count').text(guessCount + 1);
 				$('#userGuess').val('');
@@ -61,7 +71,6 @@ $(document).ready(function(){
 					$("#feedback").text("About the same");
 				}
 
-				
 				$('#count').text(guessCount + 1);
 				$('#userGuess').val('');
 				$('#guessList').append('<li>' + userinput + '</li>');
@@ -69,6 +78,7 @@ $(document).ready(function(){
 			}
 		  	
 		} else {
+
 		  	$("#feedback").text("I need a number between 1-100");
 		  	$('#userGuess').val('');
 		}
@@ -96,7 +106,7 @@ $(document).ready(function(){
 
 	$("#guessButton").click(function(e) {
   		e.preventDefault();
-		userNum = parseInt($("#userGuess").val());
+		userNum = $("#userGuess").val();
   		feedback(userNum);
 
 	});
