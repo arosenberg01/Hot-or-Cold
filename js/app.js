664028847d25ceb71e5dd2a8d3e5481121658e03
arosenberg01 <ansel01@gmail.com>
2014-12-09 00:30:59 -0800

    Basic number generation and user guess comparison

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
-
 $(document).ready(function(){
 	
-	var secretNum = "Nothing";
+	var secretNum, userNum, diff;
 
+	function generateNum() {
+	  		return Math.floor(Math.random() * 100 + 1);
+	  	}
+
+	function newGame() {
+		secretNum = generateNum();
+		console.log("secretNum is " + secretNum);
+	}
+
+
+
+	newGame();
+	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -15,28 +27,41 @@ $(document).ready(function(){
   	});
 
 
-  	// $(".new").click(function() {
+  	$(".new").click(function(e) {
+  		e.preventDefault();
+  		newGame();
+  		console.log("new game started");
 
-  	// 	secretNum = generateNum();
-  	// 	alert(secretNum);
+  	});
 
-  	// });
 
-	
+	$("#guessButton").click(function(e) {
+  		e.preventDefault();
+		
+  		userNum = parseInt($("#userGuess").val());
+  		console.log(userNum);
 
-  	function generateNum() {
-  		return Math.floor(Math.random() * 100 + 1);
-  	}
+  		userNum > secretNum ? diff = userNum - secretNum : diff = secretNum - userNum
+  		console.log("Off by " + diff);
 
-	$("#guessButton").click(function() {
-  		
-		var secretNum = generateNum();
-		console.log(secretNum);
-  		var userNum = $("#userGuess").val();
 
-  		if (secretNum == userNum) {
+	if (secretNum == userNum) {
   			$("#feedback").text("You guessed it! It's " + secretNum);
+  		} else if (diff < 10) {
+  			$("#feedback").text("Very hot");
+  		} else if (diff < 20) {
+  			$("#feedback").text("Hot");
+  		} else if (diff < 30) {
+  			$("#feedback").text("Warm");
+  		} else if (diff < 50) {
+  			$("#feedback").text("Cold");
+  		} else {
+  			$("#feedback").text("Ice cold");
   		}
+
+
+
+
   	});
 
   	
